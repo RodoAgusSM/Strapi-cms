2a1a1f6f33101bd52bc789dedaf400e423f44df5
Rodolfo Agustin Silva Messano <rodoagussm@gmail.com>
2023-09-07 16:09:06 -0300

    - Uploading files.

---
+++ src/api/cow/controllers/cow.ts
@@ -0,0 +1,7 @@
+/**
+ * cow controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::cow.cow');
---
+++ src/api/cow/routes/cow.ts
@@ -0,0 +1,7 @@
+/**
+ * cow router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::cow.cow');
